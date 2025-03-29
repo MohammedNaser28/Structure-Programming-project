0cb73ea82656267635f7deb074063fde26cd45b9
Mohammed Naser <MohammedNaser2826@gmail.com>
2025-03-29 11:51:25 +0200

    Add files

--- Recipe Managment core/Recipe Managment core.cpp
+++ Recipe Managment core/Recipe Managment core.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include "dependencies.h"
 
 int main()
 {
---
+++ Recipe Managment core/dependencies.h
@@ -0,0 +1,6 @@
+#pragma once
+//DON'T REMOVE/REPLACE ANY LIBRARIES, JUST ADD NEW ONES IF YOU NEED
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <filesystem>
