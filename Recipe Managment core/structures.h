6e2b3474e5e779f2a173f17779bd708111fa3d98
Mohammed Naser <MohammedNaser2826@gmail.com>
2025-04-15 14:21:02 +0200

    add some comments

--- Recipe Managment core/structures.h
+++ Recipe Managment core/structures.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "dependencies.h"
+struct user {
+	int id;
+	bool isAdmin= false;
+	string username;
+	string password;
+	//string name;
+	int favorites[10];
+	recipe myRecipe[10];
+};
+
+struct recipe {
+	int id;
+	string title;
+	string decription;
+	string ingrediatnes[10];
+	string imagePath;
+	float timeCoock;
+	string steps;
+	int rate;
+
+};
