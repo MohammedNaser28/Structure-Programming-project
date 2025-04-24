a5d14d20dde8dc4cde8a00416ec7e33d9d62ebb7
Mohamed Eid <mohamedeid052@gmail.com>
2025-04-24 18:03:17 +0200

    delete core project

--- Recipe Managment core/dependencies.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-//DON'T REMOVE/REPLACE ANY LIBRARIES, JUST ADD NEW ONES IF YOU NEED
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <filesystem>
-using namespace std;
--- Recipe Managment core/general.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "dependencies.h"
-#include "structures.h"
