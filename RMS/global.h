d088a23dcf3fb6105711e5ce4117a6f99a13db97
Mohammed Naser <MohammedNaser2826@gmail.com>
2025-04-29 20:54:15 +0300

    edit code 2

--- RMS/global.h
+++ RMS/global.h
@@ -3,13 +3,13 @@
 #include "dependencies.h"
 #include "structures.h"
 
-extern int num_of_users = 0;
-extern QSharedPointer<User> users[1000];
+static int num_of_users = 0;
+static QSharedPointer<User> users[1000];
 
-extern QSharedPointer<User> loged_in_user = nullptr;
+static QSharedPointer<User> loged_in_user = nullptr;
 
-extern int num_of_recipes = 0;
-extern QSharedPointer<Recipe> recipes[1000];
+static int num_of_recipes = 0;
+static QSharedPointer<Recipe> recipes[1000];
 
 // hashtable to recipes IDs to help in deleteing recipes and saving data (-1 means doesn't exist)
-extern short recipes_id_to_index[10000]; // intialized in main
+static short recipes_id_to_index[10000]; // intialized in main
