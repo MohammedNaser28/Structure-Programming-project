3711ac27ca4b5b9b2337859114431661774274ae
Mohamed Eid <mohamedeid052@gmail.com>
2025-04-29 19:48:58 +0300

    fix global.h issue

--- RMS/global.h
+++ RMS/global.h
@@ -3,13 +3,13 @@
 #include "dependencies.h"
 #include "structures.h"
 
-static int num_of_users = 0;
-static QSharedPointer<User> users[1000];
+extern int num_of_users;
+extern QSharedPointer<User> users[1000];
 
-static QSharedPointer<User> loged_in_user = nullptr;
+extern QSharedPointer<User> loged_in_user;
 
-static int num_of_recipes = 0;
-static QSharedPointer<Recipe> recipes[1000];
+extern int num_of_recipes;
+extern QSharedPointer<Recipe> recipes[1000];
 
 // hashtable to recipes IDs to help in deleteing recipes and saving data (-1 means doesn't exist)
-static short recipes_id_to_index[10000]; // intialized by (-1) in main
+extern short recipes_id_to_index[10000]; // intialized by (-1) in main
