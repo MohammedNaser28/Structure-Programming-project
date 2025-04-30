cbb90585102ff63f449cdc96d826c621589a7390
Mohamed Eid <109903701+Mohamedeid-11@users.noreply.github.com>
2025-04-30 21:21:10 +0300

    Update mainwindow.h

--- RMS/mainwindow.h
+++ RMS/mainwindow.h
@@ -18,19 +18,23 @@ public:
 
 private:
 	Ui::MainWindowClass *ui;
+    void setup_add_recipe_page();
     QGridLayout* recipes_grid;
     QList<QWidget*> recipe_pages;      // IDs of recipe pages
 
 private slots:
     void on_home_page_btn_clicked();
-
+    void on_add_recipe_page_btn_clicked();
     void on_log_out_btn_clicked();
-
     void on_save_recipe_btn_clicked();
-
     void on_display_recipe_btn_clicked();
-
     void create_recipe_page(QSharedPointer<Recipe> r_ptr);
+
+    void add_ingredient_row();
+    void add_method_row();
+    void remove_row();
+    void choose_image();
+    void on_submit_btn_clicked();
 
 signals:
     void switchToDialog();
